bed3838645e1ef7195ca05ae83346731724a646c
Tualatrix Chou <tualatrix@gmail.com>
2011-05-05 22:04:52 +0800

    Finish the basic UI and load_config feature

---
+++ setup.py
@@ -0,0 +1,20 @@
+import glob
+from setuptools import setup, find_packages
+
+setup(name='vpn-indicator',
+      version='1.0.0',
+      description='VPN Indicator for Ubuntu',
+      author='Tualatrix Chou',  
+      author_email='tualatrix@gmail.com',
+      url='http://imtx.me/',
+      scripts=['vpn-indicator', 'vpn-indicator-daemon'],
+      packages=find_packages(),
+      data_files=[
+          ('../etc/dbus-1/system.d/', ['data/vpn-indicator-daemon.conf']),
+          ('share/dbus-1/system-services', ['data/me.imtx.vpndaemon.service']),
+          ('share/applications', ['data/vpn-indicator.desktop']),
+          ],
+      license='GNU GPL',
+      platforms='linux',
+      test_suite='tests',
+)
