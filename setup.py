bb5a2d6b0fcd05adc402ff9003d5e0b2ba65f9f1
Tualatrix Chou <tualatrix@gmail.com>
2011-05-23 17:23:07 +0800

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import glob
 from setuptools import setup, find_packages
 
 setup(name='vpn-indicator',
-      version='1.0.0',
+      version='1.0.1',
       description='VPN Indicator for Ubuntu',
       author='Tualatrix Chou',  
       author_email='tualatrix@gmail.com',
